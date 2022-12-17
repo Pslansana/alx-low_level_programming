f1d62788fedf57a2bce1c0613d40394cd663798e
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2022-12-17 13:51:18 -0800

    Task ls

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,12 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
 {
-	return (write(1, &c, 1));
+	print_alphabet();
+	return (0);
 }

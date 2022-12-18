23416d4cec7b568f8c3cc956399072bc18e925e2
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2022-12-18 04:05:04 -0800

    Task 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
 /**
-* main - check the code
-*
-* Return: Always 0.
+* main - prints _putchar and a new line
+* Return: 0
 */
 int main(void)
 {
-	print_alphabet();
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
 }

0539f27ef8c3bc5f82996166adf09e305d5eef06
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2023-01-01 01:48:56 -0800

    0-main.c

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,17 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-int _isupper(int c);
-int isdigit(int c);
-int nul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_number(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int sze);
-void print_triangle(int size);
-void print_number(int n);
-
-#endif /* MAIN_H */
+#include "main.h"
+#include <stdio.h>
+/**
+* main - check the code for Jerome
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char c;
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
+}

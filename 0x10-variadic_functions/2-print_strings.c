bd327587b3c004b1f52a7b6e52025129c22fb35e
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2023-01-24 08:49:40 -0800

    Task 2. One woman's constant is another woman's variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,3 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings.
+ * @separator: string to be printed between the strings.
+ * @n: number of strings passed to the function.
+ *
+ * Return: no return.
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list valist;
